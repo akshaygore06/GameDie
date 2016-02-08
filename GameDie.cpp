9a3d6adc94dfe9a55bafc6ded54028122d2abd1b
akshaygore06 <agore1@mail.csuchico.edu>
2016-02-08 10:32:33 -0800

    Generate Random number between1 and (4 to 16)

--- GameDie.cpp
+++ GameDie.cpp
@@ -11,10 +11,12 @@ GameDie::GameDie()
 
 //generate a random number between 1-6 (inclusive) and display it
 int GameDie::roll()
-{	
+{
 	int randNumber = 0;
+	int maxNumber = 20;
+	int minNumber = 4;
 	srand(time(NULL));
-        randNumber = rand()%6+1;
-//	cout << "Random Number is :"<< randNumber<<endl;
+  randNumber = rand()%(maxNumber - minNumber +1) + minNumber; //generates number between Min Number and Max Number 
+	//cout << "Random Number is :"<< randNumber<<endl;
 return randNumber;
 }
